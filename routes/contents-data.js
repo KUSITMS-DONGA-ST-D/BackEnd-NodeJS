ab4dc9ade6beca4237bd2d21439eed7ba9a39657
seungryeol <opop0421@naver.com>
2023-03-21 03:13:19 +0900

    달 별 일정 뿌리기

--- routes/contents-data.js
+++ routes/contents-data.js
@@ -4,33 +4,44 @@ const conn = require('../config/db.js');
 const mysql = require('mysql2/promise');
 
 
-router.get('/', async (req, res, next) => {    
+router.get('/', async (req, res, next) => {
     const connection = await mysql.createConnection(conn.real); // DB 커넥션 생성
-    await connection.connect();         
-    try {  
-        const { start_day, end_day, age, gender, category} = req.body;
-        
-        const user_sign = `select * from USERS where username = '${String(get_user)}'; `
-        const result = await connection.query(user_sign);
+    await connection.connect();
+    try {
+        const { start_day, end_day, age, gender, category } = req.body;
 
-   
-        
-        const new_user = `INSERT INTO USERS(username, correct_cnt) VALUES ('${String(get_user)}', 0); `
-        await connection.query(new_user);
+        console.log(String(category));
+        console.log(start_day);
+        console.log(end_day);
+        console.log(gender);
       
-        return res.status(202).json({
-            code: 202,
-            message: "username enroll"
-        });
+
+        const contents_data = `select * from contents where (created_date between '${start_day}' and '${end_day}') and (content_category = "${category}"); `;
         
         
+        const result = await connection.query(contents_data);
+
+        console.log(result[0]);
+
+        const re = result[0].map((el)=> el.content_id);
+        console.log(re);
+
+
+        // const new_user = `INSERT INTO USERS(username, correct_cnt) VALUES ('${String(get_user)}', 0); `
+        // await connection.query(new_user);
+
+        return res.status(202).json(
+            result[0]
+        );
+
+
     } catch (error) {
         res.status(500).json({
-        "code":500,
-        "message": "server error"
+            "code": 500,
+            "message": "server error"
         });
-    }finally{
-       connection.end();  
+    } finally {
+        connection.end();
     }
 
 });
