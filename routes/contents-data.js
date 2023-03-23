cd0afb034e384d4c6df6f807aeabe0e640cea44f
seungryeol <opop0421@naver.com>
2023-03-23 11:10:27 +0900

    컨텐츠분석(댓글,관심 조회)

--- routes/contents-data.js
+++ routes/contents-data.js
@@ -27,11 +27,24 @@ router.get('/', async (req, res, next) => {
 
 
     try {
-        const contents_data = `select * from users where (gender = "${gender[0]}" or gender ="${gender[1]}") and (category = "${category[0]}" or category = "${category[1]}" or category = "${category[2]}" or category = "${category[3]}") and (${age_list[0]}<= age && age < ${age_list[1]} or  ${age_list[2]}<= age && age < ${age_list[3]} or ${age_list[4]}<= age && age < ${age_list[5]} or  ${age_list[6]}<= age && age < ${age_list[9]} or ${age_list[10]}<= age && age < ${age_list[11]}); `;
+        const user_data_query = `select * from users where (gender = "${gender[0]}" or gender ="${gender[1]}") and (category = "${category[0]}" or category = "${category[1]}" or category = "${category[2]}" or category = "${category[3]}") and (${age_list[0]}<= age && age < ${age_list[1]} or  ${age_list[2]}<= age && age < ${age_list[3]} or ${age_list[4]}<= age && age < ${age_list[5]} or  ${age_list[6]}<= age && age < ${age_list[9]} or ${age_list[10]}<= age && age < ${age_list[11]}); `;
         //const contents_data = `select * from users where ${age_list[0]}<= age && age < ${age_list[1]} or  ${age_list[2]}<= age && age < ${age_list[3]} or ${age_list[4]}<= age && age < ${age_list[5]} or  ${age_list[6]}<= age && age < ${age_list[9]} or ${age_list[10]}<= age && age < ${age_list[11]} ;`;
-        const result = await connection.query(contents_data);
-       
-        const result2 = result[0].map((el) => el.user_id);
+        const user_data = await connection.query(user_data_query);
+        const user_id = user_data[0].map((el) => el.user_id);
+
+        const comment_data_query = `select created_date from interest where created_date between "${start_day}" and "${end_day}";`;
+        const interest_data_query = `select created_date from comment where created_date between "${start_day}" and "${end_day}";`;
+
+
+        const { comment_data, interest_data} = await Promise.all([
+            connection.query(comment_data_query),
+            connection.query(interest_data_query),
+        ]);
+
+        const comment_id = comment_data[0].map((el)=> el.comment_id);
+        const interest_id = interest_data[0].map((el)=> el.interest_id)
+
+
 
 
 
@@ -39,7 +52,7 @@ router.get('/', async (req, res, next) => {
         // await connection.query(new_user);
         console.log(result2);
         return res.status(202).json(
-            result[0]
+            user_data[0]
         );
 
 
